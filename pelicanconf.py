dad5002eaba0fc273dc4e522412c88854d2e923e
40823225 <40823225@gm.nfu.edu.tw>
2019-11-07 21:16:43 +0800

    add pelican blog settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '40823225'
+SITENAME = 'cp2019 計算機程式40823225'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
